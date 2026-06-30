f902768b3e3587f78d9692b107d5ab35c70da959
crypto2115 <ysemir03@gmail.com>
2026-07-01 00:27:10 +0300

    Refactor PostCSS config to use default settings
    Simplified Tailwind CSS and Autoprefixer configurations.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {
-      config: './tailwind.config.js',
-    },
-    autoprefixer: {
-      overrideBrowserslist: [
-        'last 4 Chrome versions',
-        'last 4 Firefox versions',
-        'last 4 Safari versions',
-        'last 4 Edge versions',
-        'last 4 Opera versions',
-        'last 4 Samsung Internet versions',
-        'last 2 Android versions',
-        'last 2 iOS versions',
-      ],
-    },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
